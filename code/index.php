b838e7b34098f9aa60646853275ed63cdee9a9ae
crmf <phr6@ya.ru>
2024-10-17 05:10:56 +0200

    connected to database

---
+++ code/index.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "db";
+$username = "user";
+$password = "userpassword";
+$dbname = "adstable";
+
+// takes about 10s to intiate
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
